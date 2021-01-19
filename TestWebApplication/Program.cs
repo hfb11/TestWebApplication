519b6d2f87f67f4375f38a31a2639431f0de7719
Simon Hilner <simon.hilner@gmx.de>
2021-01-19 15:59:28 +0100

    Test

--- TestWebApplication/Program.cs
+++ TestWebApplication/Program.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 namespace TestWebApplication {
 	public class Program {
 		public static void Main(string[] args) {
+			//TEST
 			CreateHostBuilder(args).Build().Run();
 		}
 
